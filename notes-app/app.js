ec3cdf5285573ce1b8f4c37cada03d9abe927949
mtng45 <mtng.tokyo@gmail.com>
2019-10-05 01:04:08 +0900

    reading a note

--- notes-app/app.js
+++ notes-app/app.js
@@ -55,8 +55,15 @@ yargs.command({
 yargs.command({
   command: 'read',
   describe: 'read a note',
-  handler() {
-    console.log('Reading a note')
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.readNote(argv.title)
   }
 })
 

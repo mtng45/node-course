eb47d0f529bf82cf2ccf0b71434579d0f2ae69eb
mtng45 <mtng.tokyo@gmail.com>
2019-10-09 00:41:10 +0900

    HTTP Requests Without a Library

---
+++ playground/6-raw-http.js
@@ -0,0 +1,21 @@
+const https = require('https')
+const url = 'https://api.darksky.net/forecast/5e626dd2819db395be4eb5253f8edc51/40,-75'
+
+const request = https.request(url, (response) => {
+  let data = ''
+
+  response.on('data', (chunk) => {
+    data = data + chunk.toString()
+  })
+
+  response.on('end', () => {
+    const body = JSON.parse(data)
+    console.log(body)
+  })
+})
+
+request.on('error', (error) => {
+  console.log('An error', error)
+})
+
+request.end()

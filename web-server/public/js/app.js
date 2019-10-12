6aaa8c192bc14ff989fa7773e4c020d232ceb9eb
mtng45 <mtng.tokyo@gmail.com>
2019-10-13 01:45:17 +0900

    wiring up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,16 @@
 console.log('client side javascript file is loaded!')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 const fetchForecast = location => {
+
   fetch('http://localhost:3000/weather?address=' +　location).then((responce) => {
     responce.json().then(data => {
       if (data.error) {
-        console.log(data.error)
+        messageOne.textContent = data.error
       } else {
-        console.log(data.location)
-        console.log(data.forecast)
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
       }
     })
   })
@@ -18,6 +21,11 @@ const search =  document.querySelector('input')
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
+
   const location = search.value
+
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+
   fetchForecast(location)
 })

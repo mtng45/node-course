94d8f02f8b8c45fd183462f7e568887ef65f193b
mtng45 <mtng.tokyo@gmail.com>
2019-10-30 23:20:56 +0900

    Promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,45 +1,14 @@
-// Node.js API
-setTimeout(() => {
-  console.log('Two seconds are up')
-}, 2000)
-
-const names = ['Andrew', 'Jen', 'Jess']
-const shortNames = names.filter((name) => name.length < 4)
-
-const geocode = (address, callback) => {
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    const data = {
-      latitude: 0,
-      longitude: 0,
-    }
-    callback(data)
+    // callback('This is my error!', undefined)
+    callback(undefined, [1, 4, 7])
   }, 2000)
 }
 
-/**
- * const data = geocode('Philadelphia')
- * geocode 関数は、setTimeout だけしかないので、callstack が empty になり、data が undefined になる
- * 第２引数に callback 関数を入れ、geocode 内で実行
- * 値を返す代わりにコールバックを取り、setTimeout で返ってきた値でコールバックを呼び出す
- * これにより、return文と同じ目標が達成される
- */
+doWorkCallback((error , result) => {
+  if (error) {
+    return console.log(error)
+  }
 
-const data = geocode('Philadelphia', (data) => {
-  console.log(data)
-})
-
-/**
- * ある種の非同期プロセスをシュミレートするために、単純な setTimeout callback に固執する
- */
-
-const add = (x, y, callback) => {
-  setTimeout(() => {
-    // addからではなく、この内部関数から戻ってくるので、ここで return を使用することはできない
-    const sum = x + y
-    callback(sum)
-  }, 2000)
-}
-
-add(1, 4, (sum) => {
-  console.log(sum) // Should print: 5
+  console.log(result)
 })
